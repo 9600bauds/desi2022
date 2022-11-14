504d641bf1d54fe62d132132b2cda43d55a966c9
Blas <Blas@192.168.0.13>
2022-11-14 20:31:15 -0300

    Commit de prueba - Añade el POJO ClimaActual

---
+++ src/main/java/com/tsti/smn/pojos/ClimaActual.java
@@ -0,0 +1,64 @@
+package com.tsti.smn.pojos;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class ClimaActual {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private int temperatura;
+	
+	private int humedad;
+	
+	private String estado;
+	
+	@ManyToOne
+	private Ciudad ciudad;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public int getTemperatura() {
+		return temperatura;
+	}
+
+	public void setTemperatura(int temperatura) {
+		this.temperatura = temperatura;
+	}
+
+	public int getHumedad() {
+		return humedad;
+	}
+
+	public void setHumedad(int humedad) {
+		this.humedad = humedad;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public Ciudad getCiudad() {
+		return ciudad;
+	}
+
+	public void setCiudad(Ciudad ciudad) {
+		this.ciudad = ciudad;
+	}
+}
