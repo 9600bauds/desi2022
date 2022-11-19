1b203537983b65c67ed8b9838b33eef7bc6d267c
Blas <Blas@192.168.0.13>
2022-11-19 19:54:08 -0300

    Prototipo para ver pronosticos individuales

---
+++ src/main/java/com/tsti/smn/capaPresentacion/pronostico/PronosticoVerController.java
@@ -0,0 +1,31 @@
+package com.tsti.smn.capaPresentacion.pronostico;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.tsti.smn.capaServicios.PronosticoService;
+import com.tsti.smn.pojos.Ciudad;
+import com.tsti.smn.pojos.Pronostico;
+
+@Controller
+@RequestMapping("/pronosticoVer")
+public class PronosticoVerController {
+	@Autowired
+	private PronosticoService servicioPronostico;
+	
+	@RequestMapping(path = {"", "/{id}"},method=RequestMethod.GET)
+	public String mostrarPronostico(Model modelo, @PathVariable("id") Long idPronostico) {
+		Pronostico elPronostico = servicioPronostico.getById(idPronostico);
+		modelo.addAttribute("pronostico", elPronostico);
+		
+		Ciudad laCiudad = elPronostico.getCiudad();
+		modelo.addAttribute("nombreCiudad", laCiudad.getNombre());
+		
+		return "pronosticoVer";
+	}
+	
+}
