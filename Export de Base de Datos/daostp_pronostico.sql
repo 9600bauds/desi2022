3fb214e515dfea023d34b360a48f74900f6d5fbd
9600bauds <blasaladio3@gmail.com>
2022-11-20 17:53:35 -0300

    Base de datos de prueba

---
+++ Export de Base de Datos/daostp_pronostico.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
+--
+-- Host: localhost    Database: daostp
+-- ------------------------------------------------------
+-- Server version	8.0.29
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `pronostico`
+--
+
+LOCK TABLES `pronostico` WRITE;
+/*!40000 ALTER TABLE `pronostico` DISABLE KEYS */;
+INSERT INTO `pronostico` VALUES (17,5,'Un buen día como cualquier otro.',_binary '\0','2022-01-01 00:00:00.000000',0.5,22.8,1),(18,0,'Muy soleado.',_binary '\0','2022-01-02 00:00:00.000000',0.1,27.8,1),(19,0,'Muuy soleado.',_binary '\0','2022-01-03 00:00:00.000000',0.1,32.8,1),(20,2,'Nublado.',_binary '\0','2022-01-04 00:00:00.000000',0.32,21.5,1),(21,2,'Lluvia ligera.',_binary '\0','2022-01-05 00:00:00.000000',0.9,19.5,1),(22,20,'Lluvia fuerte.',_binary '\0','2022-01-06 00:00:00.000000',0.9,18.5,1),(23,70,'Lluvia torrencial.',_binary '\0','2022-01-07 00:00:00.000000',0.97,16.5,1),(24,170,'Bajo agua.',_binary '\0','2022-01-08 00:00:00.000000',0.98,16.5,1),(25,270,'Glub glub.',_binary '\0','2022-01-09 00:00:00.000000',0.98,14.5,1),(26,300,'Glub glub glub.',_binary '','2022-01-10 00:00:00.000000',0.99,14,1),(27,0,'Soleado.',_binary '\0','2022-01-01 00:00:00.000000',0.15,22,2),(28,0,'Soleadoo.',_binary '\0','2022-01-02 00:00:00.000000',0.15,23,2),(29,0,'Soleadooo.',_binary '\0','2022-01-03 00:00:00.000000',0.15,24,2),(30,0,'Soleadoooo.',_binary '\0','2022-01-04 00:00:00.000000',0.15,25,2),(31,0,'Soleadooooo.',_binary '\0','2022-01-05 00:00:00.000000',0.15,26,2),(32,0,'Soleadoooooo.',_binary '\0','2022-01-06 00:00:00.000000',0.15,27,2),(33,0,'Soleadooooooo.',_binary '\0','2022-01-07 00:00:00.000000',0.15,28,2),(34,0,'Soleadoooooooo.',_binary '\0','2022-01-08 00:00:00.000000',0.15,29,2),(35,0,'Soleadooooooooo.',_binary '\0','2022-01-09 00:00:00.000000',0.15,30,2),(36,0,'Soleadoooooooooo.',_binary '\0','2022-01-10 00:00:00.000000',0.15,31,2);
+/*!40000 ALTER TABLE `pronostico` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-07-07 21:29:30
