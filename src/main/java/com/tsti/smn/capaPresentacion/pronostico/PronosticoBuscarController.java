088b6c1ef7afdf41b09b78d5a34473fc66e05338
Blas <Blas@192.168.0.13>
2022-11-20 17:38:44 -0300

    Cambia el controlador de PronosticoBuscar para usar IDs en la URL

--- src/main/java/com/tsti/smn/capaPresentacion/pronostico/PronosticoBuscarController.java
+++ src/main/java/com/tsti/smn/capaPresentacion/pronostico/PronosticoBuscarController.java
@@ -3,7 +3,7 @@ package com.tsti.smn.capaPresentacion.pronostico;
 import java.sql.Date;
 
 import java.util.List;
-
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -13,6 +13,7 @@ import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -37,10 +38,14 @@ public class PronosticoBuscarController {
 	@Autowired
 	private ClimaActualService servicioClimaActual;
 	
-	@RequestMapping(method=RequestMethod.GET)
-	public String preparaForm(Model modelo) {
+	@RequestMapping(value = {"", "/{idCiudad}"}, method=RequestMethod.GET)
+	public String preparaForm(ModelMap modelo, @PathVariable Optional<Long> idCiudad) {
 		PronosticoBuscarForm form = new PronosticoBuscarForm();
 		modelo.addAttribute("formBean",form);
+		
+		if(idCiudad.isPresent()) {
+    		modelo = llenarModelo(modelo, idCiudad.get());
+		}
     	return "pronosticoBuscar";
  	}
 	
@@ -48,34 +53,15 @@ public class PronosticoBuscarController {
     public List<Ciudad> getAllProvincias() {
         return this.servicioCiudad.getAll();
     }
-    
-    @RequestMapping( method=RequestMethod.POST)
-    public String submit( @ModelAttribute("formBean") @Valid PronosticoBuscarForm formBean, BindingResult result, ModelMap modelo, @RequestParam String action) throws Excepcion {
+
+    @RequestMapping(method=RequestMethod.POST)
+    public String submit(@ModelAttribute("formBean") @Valid PronosticoBuscarForm formBean, BindingResult result, 
+    		ModelMap modelo, @RequestParam String action) throws Excepcion {
     	if(action.equals("Buscar"))
-    	{
-    		Long idCiudad = formBean.getCiudadSeleccionada();
-    		if(idCiudad != null) {
-	    		Ciudad laCiudad = servicioCiudad.getById(idCiudad);
-	    		
-	    		modelo.addAttribute("nombreCiudad", laCiudad.getNombre());
-	    		
-	    		ClimaActual elClima = servicioClimaActual.getByCiudadId(idCiudad);
-	    		modelo.addAttribute("climaActual", elClima);
-	
-	    		// Por motivos de simplicidad, como no sé en qué fecha se va a calificar este TP, voy a usar una fecha fija.
-	    		// Si la aplicación fuera "de verdad", habría muchos pronósticos y se usaría la fecha actual.
-	    		// Pero para solamente tener que generar 10 casos de prueba, voy a hacer de cuenta que hoy es 31/12/2021.
-	    		Date fechaInicio = Date.valueOf("2021-01-01");
-	    		Date fechaFinal = Date.valueOf("2022-01-10");
-	    		
-	    		//List<Pronostico> resultados = servicioPronostico.getAll();
-	    		List<Pronostico> resultados = servicioPronostico.pronosticosPorCiudadYFecha(laCiudad, fechaInicio, fechaFinal);
-	    		modelo.addAttribute("resultados", resultados);
-	    		
-	    		//modelo.addAttribute("fechaInicio", fechaInicio);
-	    		//modelo.addAttribute("fechaFinal", fechaFinal);
-    		}
-    		return "pronosticoBuscar";
+    	{			Long idCiudad = formBean.getCiudadSeleccionada();
+			if(idCiudad != null) {;				return "redirect:/pronosticoBuscar/" + idCiudad.toString();
+			};
+			return "pronosticoBuscar";
     	}
     	
     	else if(action.equals("Cancelar"))
@@ -84,5 +70,29 @@ public class PronosticoBuscarController {
     		return "redirect:/";
     	}
     	return "redirect:/";
+    }
+    
+    private ModelMap llenarModelo(ModelMap modelo, Long idCiudad) {
+		Ciudad laCiudad = servicioCiudad.getById(idCiudad);
+		
+		modelo.addAttribute("nombreCiudad", laCiudad.getNombre());
+		
+		ClimaActual elClima = servicioClimaActual.getByCiudadId(idCiudad);
+		modelo.addAttribute("climaActual", elClima);
+		
+		// Por motivos de simplicidad, como no sé en qué fecha se va a calificar este TP, voy a usar una fecha fija.
+		// Si la aplicación fuera "de verdad", habría muchos pronósticos y se usaría la fecha actual.
+		// Pero para solamente tener que generar 10 casos de prueba, voy a hacer de cuenta que hoy es 31/12/2021.
+		Date fechaInicio = Date.valueOf("2021-01-01");
+		Date fechaFinal = Date.valueOf("2022-01-10");
+		
+		//List<Pronostico> resultados = servicioPronostico.getAll();
+		List<Pronostico> resultados = servicioPronostico.pronosticosPorCiudadYFecha(laCiudad, fechaInicio, fechaFinal);
+		modelo.addAttribute("resultados", resultados);
+		
+		//modelo.addAttribute("fechaInicio", fechaInicio);
+		//modelo.addAttribute("fechaFinal", fechaFinal);
+    	
+    	return modelo;
     }
 }
